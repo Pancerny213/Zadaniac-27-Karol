5696f1de7571e0598c91fa2a2af6070ac0caa13f
zuzkaluzka <166519571+zuzkaluzka@users.noreply.github.com>
2024-04-15 13:37:39 +0200

    połaczenie 2 plików(cs)

--- powtórzenie/powtórzenie/Program.cs
+++ powtórzenie/powtórzenie/Program.cs
@@ -1,7 +1,13 @@
-﻿class PlikGłówny 
+﻿using powtórzenie;
+
+class PlikGłówny 
 {
+    
     static void Main(string[] args)
     {
         //to jest baza naszego projektu
+        Tablice tablica = new Tablice(); 
+        tablica.wyswietl();
+    
     }
 }
